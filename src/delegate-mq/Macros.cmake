8b077269832002921250d096607e9b11f48dc05b
BOOK-0CD3P8P8FE\endur <endurodave@gmail.com>
2025-02-14 08:42:32 -0800

    Update build

--- src/delegate-mq/Macros.cmake
+++ src/delegate-mq/Macros.cmake
@@ -1,15 +1,12 @@
 # Function to copy .dll files from vcpkg bin directory to the build output directory
-function(copy_vcpkg_dlls VCPKG_ROOT_DIR DELEGATE_APP)
-    # Get .dll and .pdb files in the vcpkg bin directory
-    file(GLOB ZMQ_BIN_FILES "${VCPKG_ROOT_DIR}/bin/libzmq*.dll" "${VCPKG_ROOT_DIR}/bin/libzmq*.pdb")
-
+function(copy_files SRC_FILES DEST_DIR)
     # Copy each DLL file to the build output directory
-    foreach(DLL_FILE ${ZMQ_BIN_FILES})
+    foreach(FILE ${SRC_FILES})
         add_custom_command(TARGET ${DELEGATE_APP} POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            ${DLL_FILE}  # Copy the DLL file
-            "${CMAKE_BINARY_DIR}/Debug"  # Destination directory
-            COMMENT "Copying ${DLL_FILE} to build output"
+            ${FILE}      # Copy files
+            ${DEST_DIR}  # Destination directory
+            COMMENT "Copying ${FILE} to build output"
         )
     endforeach()
 endfunction()
