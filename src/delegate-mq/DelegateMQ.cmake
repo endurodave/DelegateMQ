ca75ec9fee69d0470905d9d533e1c9bcfe9655b3
BOOK-0CD3P8P8FE\endur <endurodave@gmail.com>
2025-02-14 14:52:24 -0800

    Update build

--- src/delegate-mq/DelegateMQ.cmake
+++ src/delegate-mq/DelegateMQ.cmake
@@ -1,4 +1,33 @@
+# DelegateMQ cmake module 
+#
+# This module sets the following variables in your project:
+#
+#   DMQ_INCLUDE_DIR - the directory containing DelegateMQ headers.
+#   DMQ_LIB_SOURCES - the core DelegateMQ delegate library files.
+#   DMQ_PREDEF_SOURCES - the predefined supporting source code files 
+#   based on the DMQ build options.
+#
+# Set DMQ build options:
+#
+#   # Set DMQ build options. Update as necessary.
+#   set (DMQ_EXTERNAL_LIB "OFF")
+#   set (DMQ_ALLOCATOR "OFF")
+#   set (DMQ_UTIL "ON")
+#   set (DMQ_THREAD "DMQ_THREAD_STDLIB")
+#   set (DMQ_SERIALIZE "DMQ_SERIALIZE_NONE")
+#   set (DMQ_TRANSPORT "DMQ_TRANSPORT_NONE")
+#   include("${CMAKE_SOURCE_DIR}/../../../src/delegate-mq/DelegateMQ.cmake")
+#
+# Use variables to build:
+#
+#   # Collect DelegateMQ predef source files
+#   list(APPEND SOURCES ${DMQ_PREDEF_SOURCES})
+#
+#   # Add include directory
+#   include_directories(${DMQ_INCLUDE_DIR})
+
 set (DMQ_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}")
+set (DMQ_INCLUDE_DIR "${DMQ_ROOT_DIR}")
 
 if(EXISTS "${DMQ_ROOT_DIR}/Common.cmake")
     include ("${DMQ_ROOT_DIR}/Common.cmake")
--- src/delegate-mq/External.cmake
+++ src/delegate-mq/External.cmake
@@ -1,9 +1,12 @@
 # External library definitions to support remote delegates. Update the options 
 # below based on the target build platform.
 
-# Modify the options below for your target platform external libraries.
-
+# vcpkg package manager
+# https://github.com/microsoft/vcpkg
 set(VCPKG_ROOT_DIR "${DMQ_ROOT_DIR}/../../../vcpkg")
+if(NOT EXISTS "${VCPKG_ROOT_DIR}")
+    message(FATAL_ERROR "${VCPKG_ROOT_DIR} Directory does not exist. Update VCPKG_ROOT_DIR.")
+endif()
 
 # ZeroMQ library package
 # https://github.com/zeromq
@@ -12,32 +15,34 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(ZeroMQ_DIR "${VCPKG_ROOT_DIR}/installed/x64-linux/share/zeromq")
 endif()
-find_package(ZeroMQ CONFIG REQUIRED)
-if (ZeroMQ_FOUND)
-    message(STATUS "ZeroMQ found: ${ZeroMQ_VERSION}")
-else()
-    message(FATAL_ERROR "ZeroMQ not found!")
+if(DMQ_TRANSPORT STREQUAL "DMQ_TRANSPORT_ZEROMQ")
+    find_package(ZeroMQ CONFIG REQUIRED)
+    if (ZeroMQ_FOUND)
+        message(STATUS "ZeroMQ found: ${ZeroMQ_VERSION}")
+    else()
+        message(FATAL_ERROR "ZeroMQ not found!")
+    endif()
 endif()
     
 # MessagePack C++ library (msgpack.hpp)
 # https://github.com/msgpack/msgpack-c/tree/cpp_master
 set(MSGPACK_INCLUDE_DIR "${DMQ_ROOT_DIR}/../../../msgpack-c/include")
-if(NOT EXISTS "${MSGPACK_INCLUDE_DIR}")
-    message(FATAL_ERROR "${MSGPACK_INCLUDE_DIR} Directory does not exist. Update MSGPACK_INCLUDE_DIR to the correct directory.")
+if(DMQ_SERIALIZE STREQUAL "DMQ_SERIALIZE_MSGPACK" AND NOT EXISTS "${MSGPACK_INCLUDE_DIR}")
+    message(FATAL_ERROR "${MSGPACK_INCLUDE_DIR} Directory does not exist. Update MSGPACK_INCLUDE_DIR.")
 endif()
 
 # RapidJSON C++ library
 # https://github.com/Tencent/rapidjson
 set(RAPIDJSON_INCLUDE_DIR "${DMQ_ROOT_DIR}/../../../rapidjson/include")
-if(NOT EXISTS "${RAPIDJSON_INCLUDE_DIR}")
-    message(FATAL_ERROR "${RAPIDJSON_INCLUDE_DIR} Directory does not exist. Update RAPIDJSON_INCLUDE_DIR to the correct directory.")
+if(DMQ_SERIALIZE STREQUAL "DMQ_SERIALIZE_RAPIDJSON" AND NOT EXISTS "${RAPIDJSON_INCLUDE_DIR}")
+    message(FATAL_ERROR "${RAPIDJSON_INCLUDE_DIR} Directory does not exist. Update RAPIDJSON_INCLUDE_DIR.")
 endif()
 
 # FreeRTOS library
 # https://github.com/FreeRTOS/FreeRTOS
 set(FREERTOS_ROOT_DIR "${DMQ_ROOT_DIR}/../../../FreeRTOSv202212.00")
-if(NOT EXISTS "${FREERTOS_ROOT_DIR}")
-    message(FATAL_ERROR "${FREERTOS_ROOT_DIR} Directory does not exist. Update FREERTOS_ROOT_DIR to the correct directory.")
+if(DMQ_THREAD STREQUAL "DMQ_THREAD_FREERTOS" AND NOT EXISTS "${FREERTOS_ROOT_DIR}")
+    message(FATAL_ERROR "${FREERTOS_ROOT_DIR} Directory does not exist. Update FREERTOS_ROOT_DIR.")
 else()
     # Collect FreeRTOS source files
     file(GLOB FREERTOS_SOURCES 
