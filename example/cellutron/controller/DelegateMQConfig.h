41d7f25f47d657cee2b14e9fe6c0716cb5a2bee6
BOOK-0CD3P8P8FE\endur <endurodave@gmail.com>
2026-05-11 09:26:50 -0700

    Fix sample project

--- example/cellutron/controller/DelegateMQConfig.h
+++ example/cellutron/controller/DelegateMQConfig.h
@@ -10,6 +10,6 @@
 #define DMQ_DEFAULT_QUEUE_SIZE          20
 #define DMQ_MAX_WATCHDOG_THREADS        8
 #define DMQ_SEQ_HISTORY_SIZE            4
-#define DMQ_MAX_PARTICIPANTS            4
+#define DMQ_MAX_PARTICIPANTS            8
 
 #endif // _DELEGATEMQ_CONFIG_H
